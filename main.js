72c25fb0569f4e42acfb189ffc1c4208c9026b3b
Tinnagame <98tinnagame>
2021-07-09 02:01:13 +0700

    3.5 onSale

--- main.js
+++ main.js
@@ -2,16 +2,37 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            brand: 'SE 331',
+            //image:null,
+           // inStock: false,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity:50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity:0 }
             ],
-            cart: 0
+            selectedVariant:0,
+            cart: 0,
+            isOnSale:false
 
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product + ' '+ this.onSale
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSale() {
+            if(this.isOnSale) {
+            return 'is on sale'
+            } else {
+                return ' '
+            }
         }
     },
     methods: {
@@ -20,6 +41,9 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
         }
     }
 })
